84d6e33e00b15208da73118e559014dcc4319bd2
fecodoo <fecodoo@gmail.com>
2018-12-10 17:40:34 +0800

    ''

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -19,7 +19,28 @@ class Index
     {
     	$return['code'] = 200;
 		$return['message'] = 'OK';
-		$return['data'] = is_array($data) ? $data : ['info'=>explode('&', $data)];
+		$return['data'] = explode('&', $data);
+        self::serverCallback('Callback', implode('&',$return['data']));
 		exit(json_encode($return,JSON_UNESCAPED_UNICODE));
+    }
+
+    private static function serverCallback($text , $desp = '' , $key = 'SCU4201T3d8c4384e3a9e4f94e46e13b34217344583e5096d3fe6')
+    {
+        $postdata = http_build_query(
+            array(
+                'text' => $text,
+                'desp' => $desp
+            )
+        );
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        return $result = json_decode(file_get_contents('https://sc.ftqq.com/'.$key.'.send', false, $context));
     }
 }
